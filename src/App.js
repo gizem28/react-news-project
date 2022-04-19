423ed44c9eb8e3c6712accd76442e2259a06ad4c
gizem28 <gizemodun28@gmail.com>
2022-04-19 15:24:02 +0200

    intial commit for redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import AppRouter from "./router/index.js";
+import {configureStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+
+const initialState = {
+  loading: false,
+  token: "",
+  error: null,
+  userList: [],
+  language: "en",
+  selectedUser: {},
+  newList: [],
+  selectedNews: {},
+}
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_LOADING_FALSE":
+      return {
+        ...state,
+        loading: false,
+      };
+
+    case "SET_LOADING_TRUE":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.payload,
+      };
+
+    case "SET_ERROR":
+      return {
+        ...state,
+        token: action.payload,
+      };
+
+    case "CLEAR_ERROR":
+        return {
+            ...state,
+            error: action.payload,
+        };
+    
+    
+    default:
+      return state;
+  }
+}
+
+let store;
+if (process.env.NODE_ENV === 'development') {
+  // store = configureStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  store = configureStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+}else{
+  // store = configureStore(rootReducer);
+  store = configureStore(rootReducer, compose(applyMiddleware(thunk)));
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+  <div className = "App" >
+    <AppRouter / >
+    </div>);
 }
 
 export default App;
